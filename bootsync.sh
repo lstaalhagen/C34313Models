1d03ce7b5cb865d02aaeffdbaa701d38586f0cba
Lars Staalhagen <lars@staalhagen.dk>
2024-07-17 11:17:46 +0200

    20240717-1117

--- bootsync.sh
+++ bootsync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for MODELS in ModelsPart1.zip ModelsPart2.zip ModelsPart3.zip ; do
-  if wget -q -O /home/sim/ModelArchive/${MODELS} https://raw.githubusercontent.com/lstaalhagen/Course34313/main/${MODELS} ; then
+  if wget -q -O /home/sim/ModelArchive/${MODELS} https://raw.githubusercontent.com/lstaalhagen/C34313Models/main/${MODELS} ; then
     chown sim: /home/sim/ModelArchive/${MODELS}
   else
     rm -f /home/sim/ModelArchive/${MODELS}
