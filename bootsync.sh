3e142d02c4a1d92ea8ea95a6700b8182126076ea
Lars Staalhagen <lars@staalhagen.dk>
2024-07-17 10:16:09 +0200

    '20240717-1016'

---
+++ bootsync.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for MODELS in ModelsPart1.zip ModelsPart2.zip ModelsPart3.zip ; do
+  if wget -q -O /home/sim/ModelArchive/${MODELS} https://raw.githubusercontent.com/lstaalhagen/Course34313/main/${MODELS} ; then
+    chown sim: /home/sim/ModelArchive/${MODELS}
+  else
+    rm -f /home/sim/ModelArchive/${MODELS}
+  fi
+done
